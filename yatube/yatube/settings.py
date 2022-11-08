361bc37e3b796bc96faa936dbb2f6305fe177b0c
Никита Зайцев <genius.zaytsev@gmail.com>
2022-11-08 20:41:10 +0500

    git commit

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'post.apps.PostsConfig',  # приложение с постами
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
