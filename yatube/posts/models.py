564ce00f651b00e9a95dfd69418746b81f8f5d18
Никита Зайцев <genius.zaytsev@gmail.com>
2022-12-25 21:28:48 +0500

    git commit

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -15,12 +24,6 @@ class Post(models.Model):
     group = models.ForeignKey(
         Group,
         on_delete=models.CASCADE,
-        lank=True, null=True,
+        blank=True, null=True,
         related_name='posts'
     )
-
-
-class Group(models.Model):
-    title = models.TextField()
-    slug = models.TextField()
-    description = models.TextField()
